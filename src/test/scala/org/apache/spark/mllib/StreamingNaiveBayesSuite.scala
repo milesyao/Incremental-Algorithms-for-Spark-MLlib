657d218a3f05e8d336d71e86f2d1e86bfd149145
Andy Yan <yrq877325@163.com>
2015-04-15 19:27:16 +0800

    1
    1

--- src/test/scala/org/apache/spark/mllib/StreamingNaiveBayesSuite.scala
+++ src/test/scala/org/apache/spark/mllib/StreamingNaiveBayesSuite.scala
@@ -17,158 +17,98 @@
 
 package org.apache.spark.mllib.classification
 
-import scala.util.Random
-
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.scalatest.FunSuite
 
-import org.apache.spark.SparkException
+import org.apache.spark.mllib.util.TestingUtils._
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.mllib.util.{LocalClusterSparkContext, MLlibTestSparkContext}
-import org.apache.spark.util.Utils
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.TestSuiteBase
+import scala.util.Random
+import org.apache.spark.SparkException
 
 
-object NaiveBayesSuite {
+class StreamingNaiveBayesSuite extends FunSuite with TestSuiteBase {
 
-  private def calcLabel(p: Double, pi: Array[Double]): Int = {
-    var sum = 0.0
-    for (j <- 0 until pi.length) {
-      sum += pi(j)
-      if (p < sum) return j
-    }
-    -1
-  }
+  // use longer wait time to ensure job completion
+  override def maxWaitTimeMillis = 30000
 
-  // Generate input of the form Y = (theta * x).argmax()
-  def generateNaiveBayesInput(
-      pi: Array[Double],            // 1XC
-      theta: Array[Array[Double]],  // CXD
-      nPoints: Int,
-      seed: Int): Seq[LabeledPoint] = {
-    val D = theta(0).length
-    val rnd = new Random(seed)
+  test("accuracy") {
 
-    val _pi = pi.map(math.pow(math.E, _))
-    val _theta = theta.map(row => row.map(math.pow(math.E, _)))
+    //val lambda=1.0
+    val nPoints = 1000
+    val pi = Array(0.5, 0.3, 0.2)
+    val theta = Array(
+      Array(0.91, 0.03, 0.03, 0.03), // label 0
+      Array(0.03, 0.91, 0.03, 0.03), // label 1
+      Array(0.03, 0.03, 0.91, 0.03) // label 2
+    )
+    val decayFactor = 1.0
+    val model = new StreamingNaiveBayes()
 
-    for (i <- 0 until nPoints) yield {
-      val y = calcLabel(rnd.nextDouble(), _pi)
-      val xi = Array.tabulate[Double](D) { j =>
-        if (rnd.nextDouble() < _theta(y)(j)) 1 else 0
-      }
+    // generate random data for k-means
+    val testData = NaiveBayesSuite.generateNaiveBayesInput(pi, theta, nPoints, 42)
+    val testRDD = sc.parallelize(testData, 5)
+    testRDD.cache()
+    // setup and run the model training
+    val ssc = setupStreams(testRDD, (inputDStream: DStream[LabeledPoint]) => {
+      model.trainOn(inputDStream)
+      inputDStream.count()
+    })
 
-      LabeledPoint(y, Vectors.dense(xi))
-    }
-  }
+    runStreams(ssc, nPoints)
 
-  private val smallPi = Array(0.5, 0.3, 0.2).map(math.log)
-
-  private val smallTheta = Array(
-    Array(0.91, 0.03, 0.03, 0.03), // label 0
-    Array(0.03, 0.91, 0.03, 0.03), // label 1
-    Array(0.03, 0.03, 0.91, 0.03)  // label 2
-  ).map(_.map(math.log))
-
-  /** Binary labels, 3 features */
-  private val binaryModel = new NaiveBayesModel(labels = Array(0.0, 1.0), pi = Array(0.2, 0.8),
-    theta = Array(Array(0.1, 0.3, 0.6), Array(0.2, 0.4, 0.4)))
-}
-
-class NaiveBayesSuite extends FunSuite with MLlibTestSparkContext {
-
-  def validatePrediction(predictions: Seq[Double], input: Seq[LabeledPoint]) {
-    val numOfPredictions = predictions.zip(input).count {
-      case (prediction, expected) =>
-        prediction != expected.label
+    val numOfPredictions1 = testData.map(row => model.predict(row.features)).zip(testData).count {
+      case (prediction1, expected1) =>
+        prediction1 != expected1.label
     }
     // At least 80% of the predictions should be on.
-    assert(numOfPredictions < input.length / 5)
+    assert(numOfPredictions1 < testData.length / 5)
+
+    val numOfPredictions2 = model.predict(testRDD.map(_.features)).collect().zip(testRDD).count{
+      case (prediction2, expected2) =>
+      prediction2 != expected2.label
+    }
+
+    assert(numOfPredictions2 < testData.length / 5)
+
   }
 
-  test("Naive Bayes") {
-    val nPoints = 10000
-
-    val pi = NaiveBayesSuite.smallPi
-    val theta = NaiveBayesSuite.smallTheta
-
-    val testData = NaiveBayesSuite.generateNaiveBayesInput(pi, theta, nPoints, 42)
-    val testRDD = sc.parallelize(testData, 2)
-    testRDD.cache()
-
-    val model = NaiveBayes.train(testRDD)
-
-    val validationData = NaiveBayesSuite.generateNaiveBayesInput(pi, theta, nPoints, 17)
-    val validationRDD = sc.parallelize(validationData, 2)
-
-    // Test prediction on RDD.
-    validatePrediction(model.predict(validationRDD.map(_.features)).collect(), validationData)
-
-    // Test prediction on Array.
-    validatePrediction(validationData.map(row => model.predict(row.features)), validationData)
-  }
-
-  test("detect negative values") {
+  test("detect negative values"){
     val dense = Seq(
       LabeledPoint(1.0, Vectors.dense(1.0)),
       LabeledPoint(0.0, Vectors.dense(-1.0)),
-      LabeledPoint(1.0, Vectors.dense(1.0)),
       LabeledPoint(1.0, Vectors.dense(0.0)))
     intercept[SparkException] {
-      NaiveBayes.train(sc.makeRDD(dense, 2))
+      StreamingNaiveBayes.train(sc.makeRDD(dense, 2))
     }
+
     val sparse = Seq(
       LabeledPoint(1.0, Vectors.sparse(1, Array(0), Array(1.0))),
       LabeledPoint(0.0, Vectors.sparse(1, Array(0), Array(-1.0))),
-      LabeledPoint(1.0, Vectors.sparse(1, Array(0), Array(1.0))),
       LabeledPoint(1.0, Vectors.sparse(1, Array.empty, Array.empty)))
     intercept[SparkException] {
-      NaiveBayes.train(sc.makeRDD(sparse, 2))
+      StreamingNaiveBayes.train(sc.makeRDD(sparse, 2))
     }
+
     val nan = Seq(
       LabeledPoint(1.0, Vectors.sparse(1, Array(0), Array(1.0))),
       LabeledPoint(0.0, Vectors.sparse(1, Array(0), Array(Double.NaN))),
-      LabeledPoint(1.0, Vectors.sparse(1, Array(0), Array(1.0))),
       LabeledPoint(1.0, Vectors.sparse(1, Array.empty, Array.empty)))
     intercept[SparkException] {
-      NaiveBayes.train(sc.makeRDD(nan, 2))
+      StreamingNaiveBayes.train(sc.makeRDD(nan, 2))
     }
+
   }
 
-  test("model save/load") {
-    val model = NaiveBayesSuite.binaryModel
+ }
 
-    val tempDir = Utils.createTempDir()
-    val path = tempDir.toURI.toString
 
-    // Save model, load it back, and compare.
-    try {
-      model.save(sc, path)
-      val sameModel = NaiveBayesModel.load(sc, path)
-      assert(model.labels === sameModel.labels)
-      assert(model.pi === sameModel.pi)
-      assert(model.theta === sameModel.theta)
-    } finally {
-      Utils.deleteRecursively(tempDir)
-    }
-  }
-}
 
-class NaiveBayesClusterSuite extends FunSuite with LocalClusterSparkContext {
 
-  test("task size should be small in both training and prediction") {
-    val m = 10
-    val n = 200000
-    val examples = sc.parallelize(0 until m, 2).mapPartitionsWithIndex { (idx, iter) =>
-      val random = new Random(idx)
-      iter.map { i =>
-        LabeledPoint(random.nextInt(2), Vectors.dense(Array.fill(n)(random.nextDouble())))
-      }
-    }
-    // If we serialize data directly in the task closure, the size of the serialized task would be
-    // greater than 1MB and hence Spark would throw an error.
-    val model = NaiveBayes.train(examples)
-    val predictions = model.predict(examples.map(_.features))
-  }
-  
-}
+
+
+
+
+
 
